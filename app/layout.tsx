364bb77c7f027894e86f1cebcee605fc643128de
IT25102820 <it25102820@my.sliit.lk>
2026-08-22 11:09:32 +0530

    Use raw script tag for adsense crawler

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,12 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3718115976306201"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        ></script>
       </head>
       <body>
         <SecurityWrapper>
