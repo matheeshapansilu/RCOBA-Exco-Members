1c59961d7031c0b91bc093296ddfe99446be37c7
IT25102820 <it25102820@my.sliit.lk>
2026-09-02 19:59:34 +0530

    Explicitly allow AdSense crawler in robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin/login', '/login'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/login', '/login'],
+      },
+      {
+        userAgent: 'Mediapartners-Google',
+        allow: '/',
+      }
+    ],
     sitemap: 'https://rcoba-web.vercel.app/sitemap.xml',
   }
 }
